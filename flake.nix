caae3ff8ae784099c5d06bfca2a3ab432f1d1d12
s-aditya-k <adityakompella@outlook.com>
2024-02-22 23:59:05 -0800

    nix flake

---
+++ flake.nix
@@ -0,0 +1,93 @@
+{
+  description = "crate";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+    crane = {
+      url = "github:ipetkov/crane";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.rust-analyzer-src.follows = "";
+    };
+
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, crane, fenix, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+
+        inherit (pkgs) lib;
+
+        craneLib = crane.lib.${system};
+        src = craneLib.cleanCargoSource (craneLib.path ./.);
+
+        commonArgs = {
+          inherit src;
+          strictDeps = true;
+
+          buildInputs = [
+          ] ++ lib.optionals pkgs.stdenv.isDarwin [
+            pkgs.libiconv
+          ];
+          nativeBuildInputs = [ pkgs.yt-dlp ];
+        };
+
+        craneLibLLvmTools = craneLib.overrideToolchain
+          (fenix.packages.${system}.complete.withComponents [
+            "cargo"
+            "llvm-tools"
+            "rustc"
+          ]);
+        cargoArtifacts = craneLib.buildDepsOnly commonArgs;
+        my-crate = craneLib.buildPackage (commonArgs // rec {
+          inherit cargoArtifacts;
+          name = "my-crate";
+          buildDir = "target/release";
+        });
+      in {
+        checks = {
+          inherit my-crate;
+
+          my-crate-clippy = craneLib.cargoClippy (commonArgs // {
+            inherit cargoArtifacts;
+            cargoClippyExtraArgs = "--all-targets";
+          });
+
+          my-crate-doc = craneLib.cargoDoc (commonArgs // {
+            inherit cargoArtifacts;
+          });
+
+          my-crate-fmt = craneLib.cargoFmt {
+            inherit src;
+          };
+        };
+
+        packages = {
+          default = my-crate;
+          my-crate-llvm-coverage = craneLibLLvmTools.cargoLlvmCov (commonArgs // {
+            inherit cargoArtifacts;
+          });
+        };
+
+        apps.default = flake-utils.lib.mkApp {
+          drv = my-crate;
+        };
+
+        devShells.default = craneLib.devShell {
+          checks = self.checks.${system};
+          packages = (with pkgs; [
+            clippy
+            cargo-audit
+          ]);
+        };
+      });
+}
